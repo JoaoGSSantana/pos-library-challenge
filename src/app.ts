c80536b5ad150855bb2b0ec4b50c7f8821f8e364
João Gabriel de Sá Santana <joao.santana@amazonasinovare.com.br>
2024-11-28 18:04:02 -0300

    :wrench: chore:  start dotenv in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 // Middleware para parsing de JSON
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('API está funcionando!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("API está funcionando!");
 });
 
 export default app;
