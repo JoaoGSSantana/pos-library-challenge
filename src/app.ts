399cdce301b783de120a736fbff10d355b9c4132
João Gabriel de Sá Santana <joao.santana@amazonasinovare.com.br>
2024-11-29 18:11:48 -0300

    :sparkles: feat: book routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+import { BookController } from "./interfaces/controllers/BookController";
 
 dotenv.config();
 
@@ -7,6 +9,12 @@ const app = express();
 
 // Middleware para parsing de JSON
 app.use(express.json());
+
+app.post("/books", BookController.create);
+app.get("/books", BookController.getAll);
+app.get("/books/:id", BookController.getById);
+app.put("/books/:id", BookController.update);
+app.delete("/books/:id", BookController.delete);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("API está funcionando!");
