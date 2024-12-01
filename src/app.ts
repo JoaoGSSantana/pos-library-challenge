0d964efe27e53d0a4d20d3fbc43688372b52cbbc
João Gabriel Santana <joao.santana09@outlook.com>
2024-12-01 17:31:57 -0300

    :sparkles: feat: add publisher controller on app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 
 import { BookController } from "./interfaces/controllers/BookController";
+import { PublisherController } from "./interfaces/controllers/PublisherController";
 
 dotenv.config();
 
@@ -10,11 +11,18 @@ const app = express();
 // Middleware para parsing de JSON
 app.use(express.json());
 
+// Book
 app.post("/books", BookController.create);
 app.get("/books", BookController.getAll);
 app.get("/books/:id", BookController.getById);
 app.put("/books/:id", BookController.update);
 app.delete("/books/:id", BookController.delete);
+// Publisher
+app.post("/publishers", PublisherController.create);
+app.get("/publishers", PublisherController.getAll);
+app.get("/publishers/:id", PublisherController.getById);
+app.put("/publishers/:id", PublisherController.update);
+app.delete("/publishers/:id", PublisherController.delete);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("API está funcionando!");
